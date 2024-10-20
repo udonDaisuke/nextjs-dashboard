1ba5b176ab0fee5bc84c81bea31c1c7e611e7258
udonDaisuke <sato.must.breathe0101@gmail.com>
2024-10-20 19:25:19 +0900

    1st commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana, Zen_Maru_Gothic } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const zenmarugothic = Zen_Maru_Gothic({
+  weight: ['400','700'],
+  subsets: ['latin']
+});
