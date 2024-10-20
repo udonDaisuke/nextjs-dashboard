3ea9e78874cd946d514bb5a6345f81944dcc71e8
udonDaisuke <sato.must.breathe0101@gmail.com>
2024-10-21 01:40:53 +0900

    chapter7 finished

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import Image from 'next/image';
 
 
 export default function Page() {
+  
   return (
     <main className="flex min-h-screen flex-col p-6">
       {/* <div className={styles.shape}/> */}
